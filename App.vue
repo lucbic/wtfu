6fea2741bf4191c915fc9eed800bed58f3d76ee7
Lucas Bicudo <lucbic@gmail.com>
2020-02-19 18:35:22 -0300

    init

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+      <!-- <accordion
+        :data-array="accordionItems"
+        :expanded="accordionExpanded"
+      /> -->
+  </view>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    accordionItems: [
+      { title: 'kibe', content: 'frito'},
+      { title: 'coxinha', content: 'frita'},
+      { title: 'esfirra', content: 'assada'},
+    ],
+    accordionExpanded: false
+  })
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
